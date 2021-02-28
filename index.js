2d47540cdb12f9144849cfa79ecbea1fa1b5b125
Micah Banschick <micha.ban@gmail.com>
2021-02-28 20:32:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ function wakeDog(dogName, dogBreed) {
 
 function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`
 };
 
 function walfToPark(dogName, dogBreed) {
   console.log(`Walk ${dogName} the ${dogBreed} to the park`);
+  return `Walk ${dogName} the ${dogBreed} to the park`
 };
 
 function throwFrisbee(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Walk ${dogName} the ${dogBreed} to the park`
 };
 
 function walkHome(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Walk ${dogName} the ${dogBreed} to the park`
 };
 
 function unleashDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Walk ${dogName} the ${dogBreed} to the park`
 };
