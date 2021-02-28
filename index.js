c03e8d4e57b4b5a618a654778296be4fd63866d3
Micah Banschick <micha.ban@gmail.com>
2021-02-28 20:30:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+};
+
+function leashDog(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+};
+
+function walfToPark(dogName, dogBreed) {
+  console.log(`Walk ${dogName} the ${dogBreed} to the park`);
+};
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+};
+
+function walkHome(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+};
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+};
